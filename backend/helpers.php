6c9daad2afa994a8e01803300e3099f6b8324e24
Danilo Prieto <orugamultimedial@gmail.com>
2020-05-15 12:49:31 -0300

    Se separa el proyecto en BACK y FRONT
    para que trabajemos especificamente

---
+++ backend/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+/*********************************************************
+ * **** HOLA, COMENCEMOS DEFINIENDO LAS FUNCIONES DEL PROGRAMA
+ 
+ 
+ TIRO LAS PRIMERAS (ustedes sigan agregando):
+
+ * registro de usuario
+ * login de usuario
+ * buscador por hashtags
+ * chat interno cliente/vendedor
+ * geolocalizacion
+ * sistema de likes
+ 
+ */
