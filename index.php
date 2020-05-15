aa37cb46ea123b8ced9114dc04f7c777608b5742
Danilo Prieto <orugamultimedial@gmail.com>
2020-05-15 12:17:39 -0300

    comenzamos
    vamos a poner acá las ideas

---
+++ index.php
@@ -0,0 +1,16 @@
+<div>
+    <h3>Hay Equipo</h3>
+</div>
+<?php
+
+/*
+Hola muchachos
+aca empezamos con el proyecto
+adhieranse a la develop branch asi comenzamos a programar
+
+vamos a ver que sale 
+
+Saludos 
+
+Danilo
+*/
